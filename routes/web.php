abbcd16fb1900fa4f45c03b57bc8baebedf10186
mubarak23 <mubarakaminu340@gmail.com>
2020-08-01 15:14:37 +0100

    create three route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contacts', function(){
+    return "<h3>All Contact </h3>";
+})->name('contacts.index');
+
+
+Route::get('/contacts/create', function(){
+    return "<h3>Create Contact </h3>";
+})->name('contacts.create');
+
+
+Route::get('/contact/{id}', function($id){
+    return App\Contact::find($id);
+})->name('contacts.show');
